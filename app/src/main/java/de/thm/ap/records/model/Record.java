fa6bd0e19a8d356c05c229a6580a9b441adfa085
Unknown <yassirx91@googlemail.com>
2018-10-21 22:33:33 +0200

    added variables for a record

--- app/src/main/java/de/thm/ap/records/model/Record.java
+++ app/src/main/java/de/thm/ap/records/model/Record.java
@@ -3,65 +3,83 @@ package de.thm.ap.records.model;
 import java.io.Serializable;
 
 /*
- // ToDo getter + setter ausfüllen
+ / ToDo getter + setter ausfüllen
+ / Die Record-Klasse beeinhaltet alle gültigen Elemente eines Records (Eintrag) im Formular.
+ / Die Klasse definiert sowohl die Gültigkeit als auch die Ausgabe von Einträgen.
  */
 
 public class Record implements Serializable {
+    private String moduleName;
+    private double moduleNum;
+    private int id;
+    private int year;
+    private boolean summerTerm;
+    private boolean halfweight;
+    private int crp;
+    private int mark;
+
 
     // ToDo
     public String setModuleName(String data) {
-        return data;
+        moduleName = data;
+        return moduleName;
     }
 
     // ToDo
-    public String getModuleName(String data) {
-        String x = data;
-        return x;
+    public String getModuleName() {
+        return moduleName;
     }
 
-    public String setModuleNum(String data) {
-        String y = data;
-        return y;
+    public double setModuleNum(double data) {
+        moduleNum = data;
+        return moduleNum;
     }
 
-    public static int getId(Record record) {
-        return 0;
+    public double getModuleNum() {
+        return moduleNum;
+    }
+
+    public int setID(int id) {
+        crp = id;
+        return crp;
+    }
+
+    public int getId() {
+        return crp;
     }
 
     public void setYear(int i) {
     }
 
-    public void setSummerTerm(boolean b) {
-    }
-
-    public void setHalfWeighted(boolean b) {
-    }
-
-    public void setCrp(int i) {
-    }
-
-    public void setMark(int i) {
-    }
-
-    public double getModuleNum() {
-        return 0;
-    }
-
     public long getYear() {
+
         return 0;
+    }
+
+    public void setSummerTerm(boolean b) {
     }
 
     public long isSummerTerm() {
         return 0;
     }
 
+    public void setHalfWeighted(boolean b) {
+    }
+
     public long isHalfWeighted() {
         return 0;
+    }
+
+    public void setCrp(int i) {
     }
 
     public long getCrp() {
         return 0;
     }
+
+    public void setMark(int i) {
+    }
+
 
     public long getMark() {
         return 0;
