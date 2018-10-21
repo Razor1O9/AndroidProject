5103bbf5daf6c81f7fbe6ddd7181703a32e9d501
BlackHawk1912 <xblackhawk1912x@gmail.com>
2018-10-21 22:06:14 +0200

    added vars for statistics and created method for remaining credit points

--- app/src/main/java/de/thm/ap/logic/Statistic.java
+++ app/src/main/java/de/thm/ap/logic/Statistic.java
@@ -6,11 +6,26 @@ This class contains the logic to analyse and calculate performance records
 
 public class Statistic {
     // ToDo
-    // Anzahl gespeicherte Leistung
-    // Anzahl 50% Module
-    // Summe CP
+    private int recordCount = 0; // Anzahl gespeicherte Leistung
+    private int creditPointsSum = 0; // Summe CP
+    private int average = 0; // Durchschnittsnote
+    private int requiredCreditPoints; // Benötigte Gesammtsumme
+    private int fiftyPercentCount = 0;// Anzahl 50% Module
+
     // CP Gesamt - Summe = Noch notwendig
-    // Durchschnittsnote
+    public int getRemainingCreditPoints() {
+        try {
+            return requiredCreditPoints - creditPointsSum;
+        } catch (NullPointerException e){
+            throw new NullPointerException("no value for required credit points");
+        }
+    }
+
+    public int getAverage () {
+        // ToDo: Return weighted avarage grade in percent
+        return 0;
+    }
+
 
     // androidTest = In der app
     // tet = JUnit
