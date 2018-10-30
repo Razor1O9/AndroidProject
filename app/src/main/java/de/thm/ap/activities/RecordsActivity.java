01817a3f6548c802f78881fbbd3cdd01be7a2794
Unknown <yassirx91@googlemail.com>
2018-10-30 22:26:27 +0100

    added sendEmail function

--- app/src/main/java/de/thm/ap/activities/RecordsActivity.java
+++ app/src/main/java/de/thm/ap/activities/RecordsActivity.java
@@ -1,26 +1,31 @@
 package de.thm.ap.activities;
 
 import android.content.Context;
+import android.net.Uri;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-
 import de.thm.ap.R;
 import de.thm.ap.logic.Stats;
 import de.thm.ap.records.model.Record;
-
-import android.view.ActionMode;
+import android.content.Intent;
+import android.util.Log;
 import android.view.Menu;
-import android.view.MenuInflater;
 import android.view.View;
+import android.widget.Toast;
+import android.view.ActionMode;
+import android.view.MenuInflater;
 import android.widget.AbsListView;
 import android.widget.AdapterView;
 import android.widget.ListView;
 import android.widget.ArrayAdapter;
+
 import java.util.List;
+
 import de.thm.ap.persistence.RecordDAO;
+
 import android.view.MenuItem;
-import android.content.Intent;
+
 
 /*
  / This class is the main class and displays all current records in a table
@@ -59,11 +64,30 @@ public class RecordsActivity extends AppCompatActivity {
                 // Respond to clicks on the actions in the CAB
                 switch (item.getItemId()) {
                     case R.id.menu_delete:
-                        new RecordDAO().delete(selectedRecords);
+                        new RecordDAO(getBaseContext()).delete(selectedRecords);
                         mode.finish(); // Action picked, so close the CAB
                         return true;
                     case R.id.menu_send:
-                        //TODO senden
+                        Log.i("Email senden","");
+                        String[] TO = {""};
+                        String[] CC = {""};
+                        Intent emailIntent = new Intent(Intent.ACTION_SEND);
+
+                        emailIntent.setData(Uri.parse("mailto:"));
+                        emailIntent.setType("text/plain");
+                        emailIntent.putExtra(Intent.EXTRA_EMAIL, TO);
+                        emailIntent.putExtra(Intent.EXTRA_CC, CC);
+                        emailIntent.putExtra(Intent.EXTRA_SUBJECT, "Meine Leistungen");
+                        emailIntent.putExtra(Intent.EXTRA_TEXT, "INHALT DER MAIL"); // ToDo
+
+                        try {
+                            startActivity(Intent.createChooser(emailIntent, "Email senden"));
+                            finish();
+                            Log.i("Senden abgeschlossen", "");
+                        } catch (android.content.ActivityNotFoundException ex) {
+                            Toast.makeText(RecordsActivity.this, "There is no email client installed.", Toast.LENGTH_SHORT).show();
+                        }
+
                         mode.finish(); // Action picked, so close the CAB
                     default:
                         return false;
@@ -101,7 +125,7 @@ public class RecordsActivity extends AppCompatActivity {
         recordsListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
                 // Get the selected item
-                Record selectedRecord = (Record)parent.getItemAtPosition(position);
+                Record selectedRecord = (Record) parent.getItemAtPosition(position);
                 Intent startThis = new Intent(view.getContext(), RecordFormActivity.class);
                 startThis.putExtra("oldRecord", selectedRecord);
                 startActivity(startThis);
@@ -120,24 +144,24 @@ public class RecordsActivity extends AppCompatActivity {
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         Stats statistics = new Stats(records);
-        switch (item.getItemId()){
+        switch (item.getItemId()) {
             case R.id.action_add:
-            Intent i = new Intent(this, RecordFormActivity.class);
-            startActivity(i);
-            return true;
+                Intent i = new Intent(this, RecordFormActivity.class);
+                startActivity(i);
+                return true;
             case R.id.action_stats:
-            AlertDialog.Builder stats = new AlertDialog.Builder(this);
-            stats.setTitle(getString(R.string.statistics_header));
-            stats.setMessage(
+                AlertDialog.Builder stats = new AlertDialog.Builder(this);
+                stats.setTitle(getString(R.string.statistics_header));
+                stats.setMessage(
                         getString(R.string.statistics_record_count) + " " + records.size() + "\n" +
-                        getString(R.string.statistics_halfweight_record_count) + " " + statistics.getSumHalfWeighted() + "\n" +
-                        getString(R.string.statistics_sum) + " " +  statistics.getSumCrp() + "\n" +
-                        getString(R.string.statistics_crp_left) + " " + statistics.getCrpToEnd() + "\n" +
-                        getString(R.string.statistics_average) + " " + statistics.getAverageMark() + "\n"
+                                getString(R.string.statistics_halfweight_record_count) + " " + statistics.getSumHalfWeighted() + "\n" +
+                                getString(R.string.statistics_sum) + " " + statistics.getSumCrp() + "\n" +
+                                getString(R.string.statistics_crp_left) + " " + statistics.getCrpToEnd() + "\n" +
+                                getString(R.string.statistics_average) + " " + statistics.getAverageMark() + "\n"
 
                 );
-            stats.setNeutralButton("Schließen", null);
-            stats.show();
+                stats.setNeutralButton("Schließen", null);
+                stats.show();
         }
         return super.onOptionsItemSelected(item);
     }
@@ -145,5 +169,5 @@ public class RecordsActivity extends AppCompatActivity {
 
     /*
 
-    */
+     */
 }
