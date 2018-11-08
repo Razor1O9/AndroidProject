d3bc0500c9035d7c60ce36c4aa00d1bd569bdcc6
Unknown <yassirx91@googlemail.com>
2018-11-08 19:51:48 +0100

    Auto stash before merge of "master" and "AndroidProject/master"

--- contentprovidertest/src/main/java/de/thm/contentprovidertest/ContentProviderTester.java
+++ contentprovidertest/src/main/java/de/thm/contentprovidertest/ContentProviderTester.java
@@ -12,6 +12,13 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.nio.charset.Charset;
+
 public class ContentProviderTester extends AppCompatActivity {
     private static final String TAG = ContentProviderTester.class.getName();
 
@@ -22,19 +29,21 @@ public class ContentProviderTester extends AppCompatActivity {
         // Get data from content provider
         Uri uri = Uri.parse("content://de.thm.ap.records.cp/records");
         ContentResolver cr = getContentResolver();
-        String[] projection = {"id", "module_name"};
+        String[] projection = {"id", "module_name", "module_number"};
         Cursor c = cr.query(uri, projection, null, null, "module_name");
 
         String allRecords = "";
         if (c != null) {
             while (c.moveToNext()) {
-                allRecords = allRecords + "id: " + c.getLong(0) + " module name: " + c.getString(1) + "\n";
+                allRecords = allRecords + "id: " + c.getLong(0) + " module name: " + c.getString(1) + " module number: " + c.getString(2) + "\n";
             }
         }
-        TextView result = (TextView)findViewById(R.id.record_display);
+        TextView result = findViewById(R.id.record_display);
         result.setText(allRecords);
-    }
+        TextView resultCSV = findViewById(R.id.resultCSV);
+        result.setText(allRecords);
 
+<<<<<<< Updated upstream
     public void onClickBtn(View v) {
         Intent intent = new Intent(Intent.ACTION_PICK);
         intent.setType("list/record");
@@ -82,11 +91,30 @@ public class ContentProviderTester extends AppCompatActivity {
 //                return;
 //        }
 //    }
-}
-    // ToDo Create a Button (Action-Click) with startActivityForResult
+=======
+        Uri uriCSV = Uri.parse("content://de.thm.ap.raw.record");
+        ContentResolver resolver = getContentResolver();
+        try {
+            StringBuilder content = new StringBuilder();
+            InputStream is = resolver.openInputStream(uriCSV);
+            is = resolver.openInputStream(uriCSV);
 
-    /*
-    Intent
-    Action Pick
-    Data: list/record
-     */
+            BufferedReader reader = new BufferedReader(new InputStreamReader(is));
+            String line = reader.readLine();
+            while (line != null) {
+                content.append(line).append("\n");
+                line = reader.readLine();
+            }
+            resultCSV.setText(content);
+            reader.close();
+
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+>>>>>>> Stashed changes
+}
+
