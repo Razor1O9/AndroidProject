eb44730069fec66f08a9757bd3cda567e1466195
BlackHawk1912 <xblackhawk1912x@gmail.com>
2018-11-15 13:03:03 +0100

    updated code formatting anf imports

--- app/src/main/java/de/thm/ap/persistence/UpdateModulesWorker.java
+++ app/src/main/java/de/thm/ap/persistence/UpdateModulesWorker.java
@@ -1,5 +1,17 @@
 package de.thm.ap.persistence;
 
+import android.content.Context;
+import android.content.SharedPreferences;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+
+import javax.xml.transform.Result;
+
+import de.thm.ap.records.model.Module;
+
 public class UpdateModulesWorker extends Worker {
     private static final
     String MODULES_URL ="https://homepages.thm.de/~hg10187/modules.json";
@@ -10,48 +22,24 @@ public class UpdateModulesWorker extends Worker {
         try {
             connection = (HttpURLConnection) new URL(MODULES_URL).openConnection();
             connection.setIfModifiedSince(sharedPrefs.getLong("lastModified",0));
-            if (connection.getResponseCode() ==200) {
-
+            if (connection.getResponseCode() == 200) {
                 in = connection.getInputStream();
-                Module[] modules =
-                        new
-                                Gson().fromJson(
-                                new
-                                        InputStreamReader(in), Module[].
-                                        class
-                        );
+                Module[] modules = new Gson().fromJson(new InputStreamReader(in), Module[].class);
                 moduleDAO.deleteAll();
                 moduleDAO.persistAll(modules);
-                sharedPrefs.edit()
-                        .putLong(
-                                "lastModified"
-                                , connection.getLastModified())
-                        .apply();
+                sharedPrefs.edit().putLong("lastModified", connection.getLastModified()).apply();
             }
         }
-        catch
-                (IOException e) {
+        catch(IOException e) {
             e.printStackTrace();
-            return
-                    Result.
-                            FAILURE
-                    ;
+            return Result.FAILURE;
         }
-        finally
-        {
-            IOUtils.
-                    closeQuietly
-                            (in);
-            if
-                    (connection !=
-                    null
-                    ) {
+        finally {
+            IOUtils.closeQuietly(in);
+            if(connection != null ) {
                 connection.disconnect();
             }
         }
-        return
-                Result.
-                        SUCCESS
-                ;
+        return Result.SUCCESS;
     }
 }
