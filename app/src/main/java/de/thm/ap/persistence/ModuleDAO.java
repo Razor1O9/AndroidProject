50fdd6fc110ac1b7cf74d74fed15a785806654ef
BlackHawk1912 <xblackhawk1912x@gmail.com>
2018-11-15 15:20:34 +0100

    implemented ModuleDAO

--- app/src/main/java/de/thm/ap/persistence/ModuleDAO.java
+++ app/src/main/java/de/thm/ap/persistence/ModuleDAO.java
@@ -1,4 +1,30 @@
 package de.thm.ap.persistence;
 
-public class ModuleDAO {
+import android.arch.persistence.room.Dao;
+import android.arch.persistence.room.Delete;
+import android.arch.persistence.room.Insert;
+import android.arch.persistence.room.Query;
+import android.arch.persistence.room.Update;
+
+import java.util.List;
+import java.util.Optional;
+
+import de.thm.ap.records.model.Record;
+
+@Dao
+public interface ModuleDAO {
+    @Query("SELECT * FROM record")
+    List<Record> findAllModules();
+
+    @Insert
+    void persistModule(Record record);
+
+    @Update
+    int updateModule(Record record);
+
+    @Query("SELECT * FROM record WHERE id = :id")
+    Optional<Record> findModuleById(int id);
+
+    @Delete
+    void deleteModule(List<Record> recordListToDelete);
 }
